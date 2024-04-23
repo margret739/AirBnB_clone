0bb1a3e6586a102cfdd2800213896f297f403ee4
margret739 <margretandalya@gmail.com>
2024-04-23 13:02:57 -0700

    clone me

---
+++ models/city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Module for the City class."""
+
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """Represent a city.
+
+    Attributes:
+        state_id (str): the state id.
+        name (str): the name of the city."""
+
+        state_id = ""
+        name = ""
