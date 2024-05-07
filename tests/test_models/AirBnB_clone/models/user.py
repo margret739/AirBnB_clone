06528d71fbf8ef63ecc4ceacf11e17fa41e2d7a4
margret739 <margretandalya@gmail.com>
2024-05-07 15:09:27 -0700

    clone

---
+++ tests/test_models/AirBnB_clone/models/user.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Module for the User class."""
+
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """ class User that handles users' information."""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
