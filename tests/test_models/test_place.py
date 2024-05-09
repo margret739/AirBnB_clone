a2633c3795e16a960ff476bbd34752b7dbf82e69
margret739 <margretandalya@gmail.com>
2024-05-09 07:13:43 -0700

    test2

---
+++ tests/test_models/test_place.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""module for place class unittest."""
+
+import os
+import models
+import unittest
+from datetime import datetime
+from time import sleep
+from models.place import Place
+
+
+class TestPlace_instantiation(unittest.TestCase):
+    """unittest for testing instantiation of the place class."""
+
+    def setUp(self):
+        try:
+            os.rename("file.json", "tmp.json")
+        except FileNotFoundError:
+            pass
+
+    def tearDown(self):
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
+        try:
+            os.rename("tmp.json", "file.json")
+        except FileNotFoundError:
+            pass
+
+    def test_no_args_instantiates(self):
+        self.assertEqual(Place, type(Place()))
+
+    def test_new_instance_stored_in_objects(self):
+        self.assertIn(Place(), models.storage.all().values())
+
